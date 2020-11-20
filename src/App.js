4c79274e51ebcb1806bdbea39437542b8a2f2856
Silviaraju <47835293+Silviaraju@users.noreply.github.com>
2020-11-20 12:29:32 -0600

    deploy the app to react pages

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+let styles = {
+    margin: 'auto',
+    width: '750px'
+  };
+  
+function App() {
+  return (
+	<div style={styles}>
+		<Carousel>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+				<p className="legend">HONG KONG</p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
+				<p className="legend">SINGAPORE</p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
+				<p className="legend">JAPAN</p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
+				<p className="legend">LAS VEGAS</p>
+			</div>
+		</Carousel>
+	</div>
+  );
+}
+
+export default App;
